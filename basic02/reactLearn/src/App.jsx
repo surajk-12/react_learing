a44d2af47577756163029c4dd6b93bf50103008b
surajk85200 <surajk85200@gmail.com>
2024-03-30 16:35:44 +0530

    changes

---
+++ basic02/reactLearn/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import New from './New'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <New/>
+   <h1>this is a home page | Suraj Kumar</h1>
+   </>
+  )
+  
+}
+
+export default App
---
+++ basic02/reactLearn/src/New.jsx
@@ -0,0 +1,7 @@
+function New() {
+    return (
+        <h2>this is new file</h2>
+    )
+}
+
+export default New
